9456368803cb782013d9c56db5d9c72f2ddc0388
JonathanCristianEscofe <cristianjonathan218@gmail.com>
2023-09-09 15:37:00 -0300

    Initial commit

---
+++ src/TPO6/TPRAC6.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TPO6;
+
+/**
+ *
+ * @author Cristian
+ */
+public class TPRAC6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
